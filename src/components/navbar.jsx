3f3c076f0fa5a98f386cc073bb239842659929ff
Shailesh Gaikwad <shailesh.gaikwad078@gmail.com>
2025-04-11 11:32:12 +0530

    some changes

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,11 +1,9 @@
-
 export function Navbar() {
-    return (
-      <div className="bg-purple-800 text-white p-4 shadow-md">
-        <h1 className="text-xl md:text-2xl font-bold text-center">
-          Job Listing Web Application Task
-        </h1>
-      </div>
-    );
-  }
-  
+  return (
+    <div className="bg-purple-800 text-white p-4 shadow-md">
+      <h1 className="text-xl md:text-2xl font-bold text-center">
+        Job Listing Web Application Task
+      </h1>
+    </div>
+  );
+}
